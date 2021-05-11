446b897fe622a858274c597537e472e2de1dcdbc
Via Cognita <via.cognita@outlook.es>
2021-05-11 20:42:00 +0200

    Instead of spawing the actor I have to put them into the level to assign them the static meshes.

--- Source/Pomg/Private/PongGameModeBase.cpp
+++ Source/Pomg/Private/PongGameModeBase.cpp
@@ -7,6 +7,8 @@
 #include "Ball.h"
 #include "PongGameStateBase.h"
 #include "PongHUD.h"
+#include "Kismet/GameplayStatics.h"
+
 
 APongGameModeBase::APongGameModeBase()
 {
@@ -27,16 +29,31 @@ void APongGameModeBase::StartPlay()
 	UWorld* const World = GetWorld();
 	if (World)
 	{
+		
 		// Spawn the ball into the world.
-		CurrentBall = World->SpawnActor<ABall>(ABall::StaticClass(), FVector(-710.0f, 0.0f, 250.0f), FRotator::ZeroRotator);
+		CurrentBall = World->SpawnActor<ABall>(ABall::StaticClass(), FVector(0.0f, 0.0f, 250.0f), FRotator::ZeroRotator);
 		FVector LaunchDirection(0.0f, -1.0f, 0.0f);
 		CurrentBall->FireInDirection(LaunchDirection);
-
+		
+		/*
 		APaddle* playerPaddle = (APaddle*)World->GetFirstPlayerController()->GetPawn();
-		playerPaddle->SetActorLocation(FVector(0.0f, -800.0f, 250.0f));
+		playerPaddle->SetActorLocation(FVector(0.0f, -500.0f, 170.0f));
 		playerPaddle->SetActorRotation(FRotator::ZeroRotator);
 
-		AAIPaddle* aiPaddle = World->SpawnActor<AAIPaddle>(AAIPaddle::StaticClass(), FVector(0.0f, 800.0f, 250.0f), FRotator::ZeroRotator);
+		AAIPaddle* aiPaddle = World->SpawnActor<AAIPaddle>(AAIPaddle::StaticClass(), FVector(0.0f, 500.0f, 170.0f), FRotator::ZeroRotator);
+		*/
+
+		TArray<AActor*> FoundActors;
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), AAIPaddle::StaticClass(), FoundActors);
+
+		for (AActor* TActor : FoundActors)
+		{
+			AAIPaddle* AIPaddle = Cast<AAIPaddle>(TActor);
+
+			if (AIPaddle != nullptr)
+				// Do what ever you need with your MyActor
+				AIPaddle->SetGameBall(CurrentBall);
+		}
 
 		//aiPaddle->SetBall(CurrentBall);
 	}
